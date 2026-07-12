9ee4a31cd0b64ea4fd42adefbb16e01cf5fe04f0
Mohammad Zarei <mohammad.zarei1380@gmail.com>
2026-07-12 20:35:35 +0330

    Feat: Config Migration generator

---
+++ src/data-source.ts
@@ -0,0 +1,8 @@
+import 'dotenv/config';
+import { DataSource } from 'typeorm';
+export default new DataSource({
+  type: 'postgres',
+  url: process.env.DATABASE_URL,
+  entities: ['src/**/*.entity.ts'],
+  migrations: ['src/migrations/*.ts'],
+});
